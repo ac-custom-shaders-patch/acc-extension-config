21f329968ef4b79e5917ba9119a69c198663da0f
Surfin Bird <illvdg13@gmail.com>
2019-05-23 02:55:58 +0300

    new format
    watch script getting obliterated 😬

---
+++ config/cars/common/functions_base.lua
@@ -0,0 +1,15 @@
+function ParseColor(v)
+  if type(v) == "string" and v:sub(1, 1) == "#" then 
+    if #v == 7 then 
+      return { tonumber(v:sub(2, 3), 16) / 255, tonumber(v:sub(4, 5), 16) / 255, tonumber(v:sub(6, 7), 16) / 255 } 
+    end
+    if #v == 4 then 
+      return { tonumber(v:sub(2, 2), 16) / 15, tonumber(v:sub(3, 3), 16) / 15, tonumber(v:sub(4, 4), 16) / 15 } 
+    end
+    error("Invalid color: "..v)
+  end
+  if type(v) == "table" and #v == 3 and ( v[1] > 1 or v[2] > 1 or v[3] > 1 ) then 
+    return { v[1] / 255, v[2] / 255, v[3] / 255 } 
+  end
+  return v
+end
