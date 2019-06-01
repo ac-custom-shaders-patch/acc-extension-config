a533a15ca52265f5f759a6b5c0e70e6c42ee6c5d
Surfin Bird <illvdg13@gmail.com>
2019-06-02 00:52:39 +0300

    update for templates

--- config/cars/common/functions_base.lua
+++ config/cars/common/functions_base.lua
@@ -16,6 +16,12 @@ end
 
 function saturate(v)
   if v > 1 then return 1 end
-  if v < 1 then return 0 end
+  if v < 0 then return 0 end
+  return v
+end
+
+function clamp(v, min, max)
+  if v > max then return max end
+  if v < min then return min end
   return v
 end
